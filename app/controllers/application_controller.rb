46e0b3eee403109e8d7f293a069d03828a636ca0
Pete Hawkins <petey@hey.com>
2023-01-15 11:38:18 +0000

    Make sign in form work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  def signed_in?
+  def user_signed_in?
+    current_user.present?
   end
-  helper_method :signed_in?
+  helper_method :user_signed_in?
 
   def current_user
+    if session[:user_id]
+      @user ||= User.find(session[:user_id])
+    end
+  rescue => e
+    session.destroy
   end
   helper_method :current_user
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path, notice: "User not found!"
+    end
   end
 
   def destroy
+    session.destroy
+    redirect_to root_path
   end
 end
