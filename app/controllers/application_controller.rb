2ddf0f02d084424adf096c0e5f408c8fac3fb890
Pete Hawkins <petey@hey.com>
2023-01-15 11:30:45 +0000

    Add sign in form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def signed_in?
+  end
+  helper_method :signed_in?
+
+  def current_user
+  end
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions, only: %i[new create destroy]
+
   root "users#index"
 end
